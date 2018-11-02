fbe023e7e2bb86d35f56769d6759146298dbf9e3
ANS <36783010+answebdev@users.noreply.github.com>
2018-11-01 19:29:05 -0700

    updated changes

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
-var http = require("http");
-var fs = require("fs");
-var PORT = 5000;
 
 // Sets up the Express App
 // =============================================================
@@ -18,51 +15,3 @@ app.use(express.json());
 app.listen(PORT, function() {
    console.log("App listening on PORT " + PORT);
 });
-
-
-
-
-
-
-
-var server = http.createServer(handleRequest);
-
-// Start our server
-server.listen(PORT, function() {
-  // Callback triggered when server is successfully listening. Hurray!
-  console.log("Server listening on: http://localhost:" + PORT);
-});
-
-// Create a function which handles incoming requests and sends responses
-function handleRequest(req, res) {
-
-  // Capture the url the request is made to
-  var path = req.url;
-  // Depending on the URL, display a different HTML file.
-  switch (path) {
-
-    //http://localhost:5000/
-  case "/":
-    return   fs.readFile(__dirname + "/home.html", function(err, data) {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      });
-
-      //http://localhost:5000/reserve
-  case "/reserve":
-    return fs.readFile(__dirname + "/reserve.html", function(err, data) {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      });
-
-      //http://localhost:5000/tables
-      case "/tables":
-      return fs.readFile(__dirname + "/tables.html", function(err, data) {
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.end(data);
-        });
-      
-  default:
-    return display404(path, req, res);
-  }
-}
